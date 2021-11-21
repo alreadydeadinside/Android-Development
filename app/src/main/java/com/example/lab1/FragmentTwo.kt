cefdf753d704ef2866516c6e264a30e6c053ba5a
Bohdan Vakaliuk <54852515+BoleanSwift@users.noreply.github.com>
2021-11-21 23:12:11 +0200

    add clear text functionality to button(simply closed file output stream)

--- app/src/main/java/com/example/lab1/FragmentTwo.kt
+++ app/src/main/java/com/example/lab1/FragmentTwo.kt
@@ -1,11 +1,16 @@
 package com.example.lab1
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import java.io.FileOutputStream
 
 class FragmentTwo : Fragment() {
 
@@ -20,11 +25,25 @@ class FragmentTwo : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val tv : TextView = view.findViewById(R.id.tvFragmentTwo)
+        //Second Fragment
+        val tv: TextView = view.findViewById(R.id.tvFragmentTwo)
         val tvAnswer: TextView = view.findViewById(R.id.tvAnswer)
+        val openButton: Button = view.findViewById(R.id.openButton)
+        val clearTextButton: Button = view.findViewById(R.id.clearTextFileButton)
         val answerText = arguments?.getString("answer")
         val text = arguments?.getString("getInfo")
         tv.text = text
         tvAnswer.text = answerText
+        clearTextButton.setOnClickListener {
+            val fileName = "Example.txt"
+            val fileOutputStream: FileOutputStream
+            fileOutputStream = context?.openFileOutput(fileName, Context.MODE_PRIVATE)!!
+            fileOutputStream.close()
+            Toast.makeText(context, "File successfully cleared!", Toast.LENGTH_SHORT).show()
+
+        }
+        openButton.setOnClickListener {
+           findNavController().navigate(R.id.fragmentThree)
+        }
     }
 }
